649fb2f54a29071605f4bdd955b833a3ce1a6bb5
KKKhg <lionskang@gmail.com>
2023-03-06 21:23:37 +0900

    [ADD] CORS

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -54,5 +54,9 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
+
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin,X-Requested-With, Content-Type, Accept, Access-Control-Requested-Method, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT, DELETE");
     }
 }
