50462f3767bc6e0b950a042d20d37a2c53635b6e
KKKhg <lionskang@gmail.com>
2023-03-13 02:53:58 +0900

    [ADD] 방명록 작업중

--- app/Models/Wedding/CommentModel.php
+++ app/Models/Wedding/CommentModel.php
@@ -15,7 +15,7 @@ class CommentModel extends Model {
 
     protected $returnType     = 'array';
 
-    protected $allowedFields = ['idx', 'author', 'password', 'comment', 'deleted_at'];
+    protected $allowedFields = ['idx', 'author', 'password', 'comment', 'created_at', 'deleted_at'];
 
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
